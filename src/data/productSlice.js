e952547ac40e2eafc9b72c8b67ca6538cdcb162e
bas11111 <brouwer.bas@gmail.com>
2024-09-06 14:40:22 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    jipperdepip

---
+++ src/data/productSlice.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const fetchAllProducts = createAsyncThunk('fectch-all-products', async (apiUrl) => {
+    const response = await fetch(apiUrl)
+    return response.json()
+})
+
+const productSlice = createSlice({
+    name: 'products',
+    initialState: { data: [], fetchStatus: ''},
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.
+            addCase(fetchAllProducts.fulfilled, (state, action) => {
+                state.data = action.payload
+                state.fetchStatus = 'success'
+            })
+            .addCase(fetchAllProducts.pending, (state) => {
+                state.fetchStatus = 'loading'
+            })
+            .addCase(fetchAllProducts.rejected, (state) => {
+                state.fetchStatus = 'error'
+            })
+    }, 
+})
+
+export default productSlice
